9166e0da4723253bc03ba4f4102195f8dfca0526
Francesco-Carbone <fcarbone054@gmail.com>
2026-04-07 18:51:26 +0200

    Add files via upload

---
+++ Assets/Scripts/StressTester.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StressTester : MonoBehaviour
+{
+    [Header("Stress CPU (Calcoli Pesanti)")]
+    public bool stressCPU = false;
+    public int intensitaCalcolo = 1000000;
+
+    [Header("Stress GPU (Rendering Pesante)")]
+    public bool stressGPU = false;
+    public GameObject oggettoPesante;
+    private List<GameObject> istanzeGPU = new List<GameObject>();
+
+    void Update()
+    {
+        // Simulazione stress CPU
+        if (stressCPU)
+        {
+            // Esegue calcoli matematici inutili per occupare il thread principale
+            for (int i = 0; i < intensitaCalcolo; i++)
+            {
+                float valore = Mathf.Sqrt(Random.value) * Mathf.Exp(Random.value);
+            }
+        }
+
+        // Simulazione stress GPU
+        if (stressGPU && istanzeGPU.Count < 10000)
+        {
+            // Crea velocemente oggetti complessi per aumentare Draw Calls e Vertici
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                go.transform.position = Random.insideUnitSphere * 10;
+                istanzeGPU.Add(go);
+            }
+        }
+        else if (!stressGPU && istanzeGPU.Count > 0)
+        {
+            // Pulizia quando disattiva il test
+            foreach (GameObject obj in istanzeGPU) Destroy(obj);
+            istanzeGPU.Clear();
+        }
+    }
+}
